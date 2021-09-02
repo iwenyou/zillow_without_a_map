908c812f758741e66a1bd701115f09996aa7916d
iwenyou <iwenyou@gmail.com>
2021-09-02 14:44:17 -0700

    Created Property Model, set up csv import through db seed.

---
+++ db/migrate/20210902203218_create_properties.rb
@@ -0,0 +1,32 @@
+class CreateProperties < ActiveRecord::Migration[6.1]
+  def change
+    create_table :properties do |t|
+      t.string :sale_type
+      t.date :sold_date
+      t.string :property_type
+      t.string :city
+      t.string :address
+      t.string :state
+      t.integer :zip
+      t.integer :price
+      t.integer :beds
+      t.string :location
+      t.integer :sqft
+      t.integer :lot_size
+      t.integer :year_built
+      t.integer :days_on_market
+      t.integer :price_per_sqft
+      t.integer :hoa
+      t.string :source
+      t.integer :mls
+      t.boolean :favorite
+      t.boolean :interested
+      t.float :latitude
+      t.float :longitude
+      t.string :open_house_start_time
+      t.string :open_house_end_time
+
+      t.timestamps
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,48 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+#read the csv from lib/seeds/
+csv_content = File.read(Rails.root.join('lib', 'seeds', 'redfin_listing_090221.csv'))
+
+#parse the csv file without headers
+csv = CSV.parse(csv_content, :headers => true)
+
+#populate the data to the Property db
+counter = 0
+
+csv.each do |property|
+
+  p = Property.new
+  p.sale_type = property['SALE TYPE']
+  p.sold_date = property['SOLD DATE']
+  p.property_type = property['PROPERTY TYPE']
+  p.address = property['ADDRESS']
+  p.city = property['CITY']
+  p.state = property['STATE OR PROVINCE']
+  p.zip = property['ZIP OR POSTAL CODE']
+  p.price = property['PRICE']
+  p.beds = property['BEDS']
+  p.location = property['LOCATION']
+  p.sqft = property['SQUARE FEET']
+  p.lot_size = property['LOT SIZE']
+  p.year_built = property['YEAR BUILT']
+  p.days_on_market = property['DAYS ON MARKET']
+  p.price_per_sqft = property['$/SQUARE FEET']
+  p.hoa = property['HOA/MONTH']
+  p.source = property['SOURCE']
+  p.mls = property['MLS#']
+  p.favorite = property['FAVORITE'] == 'Y' ? true : false
+  p.interested = property['INTERESTED'] == 'Y' ? true : false
+  p.latitude = property['LATITUDE']
+  p.longitude = property['LONGITUDE']
+  p.open_house_start_time = property['NEXT OPEN HOUSE START TIME']
+  p.open_house_end_time = property['NEXT OPEN HOUSE END TIME']
+  p.save
+
+  counter += 1
+  puts "Home address: #{p.address}, #{p.city} has been saved"
+end
+
+puts "#{counter}properties has been saved"
